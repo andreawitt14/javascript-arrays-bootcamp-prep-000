b598b267e21a9f17d8f6f3b71cee752c8671a85a
Andrea Witt <andreawitt14@hotmail.com>
2018-02-23 17:18:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,7 @@ function addElementToEndOfArray(array, element){
 
 function destructivelyAddElementToEndOfArray(array, element){
   
-  return array.push(element)
+  array.push(element)
+  return array
   
 }
