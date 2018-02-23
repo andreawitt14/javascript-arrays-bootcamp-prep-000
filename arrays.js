3e7bc7034a62b75d48e5f0be8329635517bdfd33
Andrea Witt <andreawitt14@hotmail.com>
2018-02-23 17:43:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -52,6 +52,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  var newarray = array.slice(array.length - 1)
+  var newarray = array.slice(0, array.length - 1)
   return newarray
 }
